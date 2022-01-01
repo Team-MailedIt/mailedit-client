616337c44d3261de8a9de663c35e456c599e2359
Joohyun Kim <corinthionia@gmail.com>
2022-01-02 02:37:07 +0900

    :bento: [chore]: add color constants

---
+++ src/constants/colors.js
@@ -0,0 +1,39 @@
+const COLORS = {
+  /* Main Colors */
+  primary: "#5C7CFA",
+
+  indigo0: "#EEF1FE",
+  indigo2: "#BAC8FF",
+  indigo4: "#ADBDFC",
+  indigo9: "#5C7CFA",
+
+  /* Text, Component Colors */
+  gray0: "#F8F9FA",
+  gray1: "#F1F2F5",
+  gray2: "#E9ECEF",
+  gray3: "#DEE2E6",
+  gray4: "#CED4DA",
+  gray5: "#ADB5BD",
+  gray6: "#868E96",
+  gray7: "#495057",
+  gray8: "#343A40",
+  gray9: "#212529",
+
+  /* Text Colors */
+  UIBlack: "#000000",
+  UIWhite: "#FFFFFF",
+
+  /* Background Colors */
+  backgroundWhite: "#FFFFFF",
+  sidebar: "#5C7CFA",
+
+  /* Tag Colors */
+  tagBlue: "#3138FF", // Blue-c
+  tagPink: "#E64980", // Pink-c
+  tagGreen: "#38D9A9", // Green-c
+  tagYellow: "#FAB005", // Yellow-c
+  tagBlue: "#15AABF", // Blue-c
+  tagPurple: "#BE4BDB", // Purple-c
+};
+
+export default COLORS;
