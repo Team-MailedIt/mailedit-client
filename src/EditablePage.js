bdb6555254fa2a4d58039b03eebdbe247663cd49
Joohyun Kim <corinthionia@gmail.com>
2022-01-02 03:15:09 +0900

    :sparkles: [feat]: add component drag and drop

--- src/EditablePage.js
+++ src/EditablePage.js
@@ -1,7 +1,10 @@
-import { useState } from 'react';
-import EditableBlock from './EditableBlock';
-import uid from './utils/uid';
-import fetchedData from './data.json';
+import { useState } from "react";
+import EditableBlock from "./EditableBlock";
+import uid from "./utils/uid";
+import fetchedData from "./data.json";
+
+import styled from "styled-components";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 const EditPage = () => {
   // const initialBlock = { id: uid(), html: '', tag: 'p', flag: 'false' };
@@ -20,7 +23,7 @@ const EditPage = () => {
   };
 
   const addBlockHandler = (currentBlock) => {
-    const newBlock = { id: uid(), html: '', tag: 'p', flag: 'false' };
+    const newBlock = { id: uid(), html: "", tag: "p", flag: "false" };
     const index = blocks.map((b) => b.id).indexOf(currentBlock.id);
     const updatedBlocks = [...blocks];
     updatedBlocks.splice(index + 1, 0, newBlock);
@@ -57,72 +60,108 @@ const EditPage = () => {
     if (prevHtml.length === 0 && nextHtml.length === 0) {
       updatedBlocks[index] = {
         ...updatedBlocks[index],
-        flag: 'true',
+        flag: "true",
       };
     } else if (prevHtml.length === 0 && nextHtml.length !== 0) {
-      const updateBlock = { id: uid(), html: newHtml, tag: 'p', flag: 'true' };
-      const newBlock = { id: uid(), html: nextHtml, tag: 'p', flag: 'false' };
+      const updateBlock = { id: uid(), html: newHtml, tag: "p", flag: "true" };
+      const newBlock = { id: uid(), html: nextHtml, tag: "p", flag: "false" };
       updatedBlocks.splice(index, 1, updateBlock);
       updatedBlocks.splice(index + 1, 0, newBlock);
     } else if (prevHtml.length !== 0 && nextHtml.length === 0) {
       const updateBlock = {
         id: uid(),
         html: prevHtml,
-        tag: 'p',
-        flag: 'false',
+        tag: "p",
+        flag: "false",
       };
-      const newBlock = { id: uid(), html: newHtml, tag: 'p', flag: 'true' };
+      const newBlock = { id: uid(), html: newHtml, tag: "p", flag: "true" };
       updatedBlocks.splice(index, 1, updateBlock);
       updatedBlocks.splice(index + 1, 0, newBlock);
     } else if (prevHtml.length !== 0 && nextHtml.length !== 0) {
       const updateBlock = {
         id: uid(),
         html: prevHtml,
-        tag: 'p',
-        flag: 'false',
+        tag: "p",
+        flag: "false",
       };
       updatedBlocks.splice(index, 1, updateBlock);
-      const newBlock = { id: uid(), html: newHtml, tag: 'p', flag: 'true' };
+      const newBlock = { id: uid(), html: newHtml, tag: "p", flag: "true" };
       updatedBlocks.splice(index + 1, 0, newBlock);
-      const nextBlock = { id: uid(), html: nextHtml, tag: 'p', flag: 'false' };
+      const nextBlock = { id: uid(), html: nextHtml, tag: "p", flag: "false" };
       updatedBlocks.splice(index + 2, 0, nextBlock);
     }
     setBlocks(updatedBlocks);
   };
 
+  // 드래그 앤 드랍 시 배열 내 순서 재배치
+  const handleDndChange = (result) => {
+    if (!result.destination) return;
+
+    const items = [...blocks];
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    setBlocks(items);
+  };
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        width: '450px',
-        minHeight: '580px',
-        margin: '24px',
-        paddingTop: '24px',
-        border: '1px solid black',
-        borderRadius: '2px',
-        backgroundColor: '#F1F3F5',
-      }}
-    >
-      {blocks.map((block) => {
-        return (
-          // EditableBlock argument로 받을 수 있는게 string이고,
-          // boolean을 받는 속성이 따로 있음.
-          // flag를 boolean type으로 굳이 할 수는 있는데 에러 나옴.
-          <EditableBlock
-            key={block.id}
-            id={block.id}
-            tag={block.tag}
-            html={block.html}
-            flag={block.flag}
-            updatePage={updatePageHandler}
-            addBlock={addBlockHandler}
-            deleteBlock={deleteBlockHandler}
-            updateBlock={updateBlockHandler}
-          />
-        );
-      })}
-    </div>
+    <DragDropContext onDragEnd={handleDndChange}>
+      <Droppable droppableId="todosDroppable">
+        {(provided) => (
+          <Box {...provided.droppableProps} ref={provided.innerRef}>
+            {blocks.map(({ id, tag, html, flag }, index) => (
+              <Draggable key={id} draggableId={id} index={index}>
+                {(provided) => (
+                  <Wrapper
+                    ref={provided.innerRef}
+                    {...provided.dragHandleProps}
+                    {...provided.draggableProps}
+                    key={id}
+                  >
+                    <DragBtn>↕️</DragBtn>
+                    <EditableBlock
+                      id={id}
+                      tag={tag}
+                      html={html}
+                      flag={flag}
+                      updatePage={updatePageHandler}
+                      addBlock={addBlockHandler}
+                      deleteBlock={deleteBlockHandler}
+                      updateBlock={updateBlockHandler}
+                    />
+                  </Wrapper>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </Box>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 };
+
+const Box = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 450px;
+  min-height: 580px;
+  margin: 24px;
+  padding-top: 24px;
+  border: 1px solid black;
+  border-radius: 2px;
+  background: #ffffff;
+`;
+
+const DragBtn = styled.div`
+  height: 100%;
+  margin-left: 10px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
 export default EditPage;
