cbb587530b9b904bb92eb4519413b80607b2fca2
Hyunjae Kim <peterhyunjae@naver.com>
2022-01-07 14:32:12 +0900

    💄 [style]: sidebar

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import { Route, Routes } from 'react-router';
 import Landing from './pages/Landing';
 import Home from './pages/Home';
 import WorkSpace from './pages/WorkSpace';
+import Sidebar from './components/sidebar/Sidebar';
 
 const App = () => {
   return (
-    <>
+    <div style={{ display: 'flex' }}>
+      <Sidebar />
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/home" element={<Home />} />
         <Route path="/workspace" element={<WorkSpace />} />
       </Routes>
-    </>
+    </div>
   );
 };
 
--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -5,24 +5,18 @@ const GlobalStyle = createGlobalStyle`
 ${normalize}
 
 html,
-body {
-  height: 100vh;
-  width: 100vw;
+  body {
+    height: 100vh;
+    width: 100vw;
+    
+    margin: 0;
+    padding: 0;
 
-  margin: 0;
-  padding: 0;
-
-  /* Center Container */
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color:'#FAFAFA';
-}
-
-a{
-  text-decoration-line : none;
-  color: black;
-}
+  }
+  a {
+  text-decoration-line: none;
+  color:black
+  }
 
 p{
   display: block;
