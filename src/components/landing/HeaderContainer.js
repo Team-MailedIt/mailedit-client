ffafbb1d5df5feb45c101d39c8ce3cfd0762b660
Hyunjae Kim <peterhyunjae@naver.com>
2022-01-29 01:46:23 +0900

    💩 [chore] : byebye

--- src/components/landing/HeaderContainer.js
+++ src/components/landing/HeaderContainer.js
@@ -34,27 +34,29 @@ const HeaderContainer = () => {
 
   return (
     <Container>
-      <Header>
-        <MainLogo onClick={goToLanding} src="./img/mainlogo.png" />
-        {userName ? (
-          <RightContainer style={{ justifyContent: 'flex-end' }}>
-            <SpanLink onClick={handleLogout}>로그아웃</SpanLink>
-          </RightContainer>
-        ) : (
-          <RightContainer>
-            <SpanLink onClick={goToHome} style={{ marginRight: '32px' }}>
-              둘러보기
-            </SpanLink>
-            <VerticalLine style={{ marginRight: '32px' }} />
-            <SpanLink
-              style={{ marginRight: '32px' }}
-              onClick={handleSignInBtnClick}
-            >
-              로그인
-            </SpanLink>
-            <SignUp onClick={handleSignUpBtnClick}>지금 시작하기</SignUp>
-          </RightContainer>
-        )}
+      <Header src="./img/mainGif.gif">
+        <Wrapper>
+          <MainLogo onClick={goToLanding} src="./img/mainlogo.png" />
+          {userName ? (
+            <RightContainer style={{ justifyContent: 'flex-end' }}>
+              <SpanLink onClick={handleLogout}>로그아웃</SpanLink>
+            </RightContainer>
+          ) : (
+            <RightContainer>
+              <SpanLink onClick={goToHome} style={{ marginRight: '32px' }}>
+                둘러보기
+              </SpanLink>
+              <VerticalLine style={{ marginRight: '32px' }} />
+              <SpanLink
+                style={{ marginRight: '32px' }}
+                onClick={handleSignInBtnClick}
+              >
+                로그인
+              </SpanLink>
+              <SignUp onClick={handleSignUpBtnClick}>지금 시작하기</SignUp>
+            </RightContainer>
+          )}
+        </Wrapper>
       </Header>
       <SignInModal
         isSignInModalOpen={isSignInModalOpen}
@@ -66,31 +68,40 @@ const HeaderContainer = () => {
         setIsSignInModalOpen={setIsSignInModalOpen}
         setIsSignUpModalOpen={setIsSignUpModalOpen}
       />
-      <MainGif src="./img/mainGif.gif" />
+      {/* <MainGif src="./img/mainGif.gif" /> */}
     </Container>
   );
 };
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-right: 48px;
+  margin-left: 48px;
+  margin-top: 36px;
+  @media screen and (max-width: 1440px) {
+    margin-top: 28px;
+  }
+`;
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   width: 100vw;
   height: auto;
-  background: linear-gradient(126.85deg, #2d53db 41.71%, #93aaff 90.44%);
 `;
 const Header = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
 
-  justify-content: space-between;
-  align-items: center;
-  margin-right: 48px;
-  margin-left: 48px;
-
-  height: 116px;
+  height: 588px;
   @media screen and (max-width: 1440px) {
-    height: 88px;
+    height: 444px;
   }
+
+  background-image: ${({ src }) => `url(${src})`};
+  background-size: contain;
 `;
 const RightContainer = styled.div`
   display: flex;
