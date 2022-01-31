2464eb5def0fb588e2ecb1ac0987766f5ef3291a
Hyunjae Kim <peterhyunjae@naver.com>
2022-02-01 01:44:33 +0900

    ⚡️ [refactor] : svg loading error fixed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import { Route, Routes } from "react-router";
-import Landing from "./pages/Landing";
-import Home from "./pages/Home";
-import WorkSpace from "./pages/WorkSpace";
-import SelectTemplateProvider from "./contexts/SelectTemplateContext";
-import ContentProvider from "./contexts/ContentContext";
-import AuthProvider from "./contexts/AuthContext";
+import { Route, Routes } from 'react-router';
+import Landing from './pages/Landing';
+// import Home from "./pages/Home";
+// import WorkSpace from './pages/WorkSpace';
+import SelectTemplateProvider from './contexts/SelectTemplateContext';
+import ContentProvider from './contexts/ContentContext';
+import AuthProvider from './contexts/AuthContext';
+import { lazy, Suspense } from 'react';
+
+const Home = lazy(() => import('./pages/Home'));
+const WorkSpace = lazy(() => import('./pages/WorkSpace'));
 
 const App = () => {
   return (
-    <AuthProvider>
-      <SelectTemplateProvider>
-        <ContentProvider>
-          <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/workspace" element={<WorkSpace />} />
-          </Routes>
-        </ContentProvider>
-      </SelectTemplateProvider>
-    </AuthProvider>
+    <Suspense fallback={<div>Loading...</div>}>
+      <AuthProvider>
+        <SelectTemplateProvider>
+          <ContentProvider>
+            <Routes>
+              <Route path="/" element={<Landing />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/workspace" element={<WorkSpace />} />
+            </Routes>
+          </ContentProvider>
+        </SelectTemplateProvider>
+      </AuthProvider>
+    </Suspense>
   );
 };
 
