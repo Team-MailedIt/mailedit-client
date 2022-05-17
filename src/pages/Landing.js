f6ff23550b30ed0d5702f4549be3653810baf07d
Joohyun Kim <corinthionia@gmail.com>
2022-05-17 16:31:52 +0900

    :fire: [refactor] reset landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,35 +1,13 @@
-import HeaderContainer from '../components/landing/HeaderContainer';
-import FooterContainer from '../components/landing/FooterContainer';
 import styled from 'styled-components';
 import MobileModal from '../components/landing/MobileModal';
-import BodyFirst from '../components/landing/BodyFirst';
-import BodySecond from '../components/landing/BodySecond';
-import BodyThird from '../components/landing/BodyThird';
-import BodyForth from '../components/landing/BodyForth';
-import BodyFifth from '../components/landing/BodyFifth';
 
 const Landing = () => {
   return (
     <>
       <MobileModal />
-      <Container>
-        <HeaderContainer />
-        <BodyFirst />
-        <BodySecond />
-        <BodyThird />
-        <BodyForth />
-        <BodyFifth />
-        <FooterContainer />
-      </Container>
+      <div>Hi</div>
     </>
   );
 };
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
 
 export default Landing;
